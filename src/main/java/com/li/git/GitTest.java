d43a3c35c978a8afdfd93077a7ab77c4afdc4a3b
lzr <lzr@li.com>
2022-08-07 11:04:54 +0800

    first commit

---
+++ src/main/java/com/li/git/GitTest.java
@@ -0,0 +1,12 @@
+package com.li.git;
+
+/**
+ * @author lzr
+ * @Description
+ * @create 2022--08--07--11:00
+ */
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("hello git!");
+    }
+}
